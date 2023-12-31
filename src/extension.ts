5057a9f6ab21433f869b20dc0f10973d5e23be78
Julia <email96123@gmail.com>
2023-12-30 23:05:35 -0700

    update extension to accept custom actions and commands

--- src/extension.ts
+++ src/extension.ts
@@ -1,81 +1,39 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
+import runCommands from "./utils/runCommands";
+import getConfigFile from "./utils/getConfigFile";
+
 import * as vscode from "vscode";
-import path from "path";
-import fs from "fs";
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
-  // Use the console to output diagnostic information (console.log) and errors (console.error)
-  // This line of code will only be executed once when your extension is activated
+function activate(context: vscode.ExtensionContext) {
   console.log('Congratulations, your extension "auto-term" is now active!');
+  const configFile = getConfigFile();
 
-  // The command has been defined in the package.json file
-  // Now provide the implementation of the command with registerCommand
-  // The commandId parameter must match the command field in package.json
+  if (!configFile) {
+    return;
+  }
+
   let disposable = vscode.commands.registerCommand(
-    "extension.openTerminals",
-    () => {
-      openTerminals();
+    "extension.manageTerminals",
+    (action) => {
+      if (configFile[action]) {
+        runCommands(configFile, action);
+      } else {
+        vscode.window.showErrorMessage(
+          `Action not found in the configuration file: ${action}`
+        );
+      }
     }
   );
 
   context.subscriptions.push(disposable);
 }
 
-function openTerminals() {
-  const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
-
-  if (!workspaceFolder) {
-    vscode.window.showErrorMessage("No workspace found.");
-    return;
-  }
-
-  const configFile = path.join(
-    workspaceFolder.uri.fsPath,
-    "terminalConfigurations.json"
-  );
-
-  if (!fs.existsSync(configFile)) {
-    vscode.window.showErrorMessage(
-      "terminalConfigurations.json not found in the workspace."
-    );
-    return;
-  }
-
-  try {
-    const terminalConfigurations = require(configFile);
-
-    if (!Array.isArray(terminalConfigurations)) {
-      vscode.window.showErrorMessage(
-        "Invalid terminalConfigurations file format."
-      );
-      return;
-    }
-
-    terminalConfigurations.forEach(
-      (config: { name: string; command: string }) => {
-        if (
-          typeof config.name === "string" &&
-          typeof config.command === "string"
-        ) {
-          const terminal = vscode.window.createTerminal({ name: config.name });
-          terminal.show();
-          terminal.sendText(config.command);
-        } else {
-          vscode.window.showErrorMessage(
-            `Invalid terminal configuration format. ${config}`
-          );
-        }
-      }
-    );
-  } catch (error) {
-    vscode.window.showErrorMessage(
-      `Error reading terminalConfigurations: ${error}`
-    );
-  }
-}
-
 // This method is called when your extension is deactivated
-export function deactivate() {}
+function deactivate() {}
+
+// Export the activate and deactivate functions
+module.exports = {
+  activate,
+  deactivate,
+};
