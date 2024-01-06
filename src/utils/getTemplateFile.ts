90d95a1131334ecbf23bf7e8320abe20cb202d9d
Julia <email96123@gmail.com>
2024-01-06 15:49:59 -0700

    Remove console logs

--- src/utils/getTemplateFile.ts
+++ src/utils/getTemplateFile.ts
@@ -1,22 +1,16 @@
-import * as vscode from "vscode";
-import * as path from "path";
 import * as fs from "fs";
+import * as path from "path";
 
-export default function getTemplateFile(templateName: string) {
+export default function getTemplateFile(templateName: string): string {
   const extensionPath = path.join(__dirname, "..");
-
   const templateDir = path.join(extensionPath, "templates");
-
-  console.log("templateDir", templateDir);
-
   const templateFilePath = path.join(templateDir, templateName);
-
-  console.log("templateFilePath", templateFilePath);
 
   if (!fs.existsSync(templateFilePath)) {
     throw new Error(`Template file '${templateName}' not found.`);
   }
 
   const templateContent = fs.readFileSync(templateFilePath, "utf8");
+
   return templateContent;
 }
