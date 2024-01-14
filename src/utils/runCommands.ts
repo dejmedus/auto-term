3cd09f80352d7572b0935d2ff5e6ed4f0b9c07dd
Julia <email96123@gmail.com>
2024-01-14 13:00:17 -0700

    fix: new terms should accept custom commands

--- src/utils/runCommands.ts
+++ src/utils/runCommands.ts
@@ -24,14 +24,19 @@ export default function runCommands(tabConfig: any) {
         });
         newTerminal.show();
         commands.forEach((command: string) => {
-          newTerminal.sendText(command);
+          if (customCommands[command.toLowerCase()]) {
+            console.log("custom command found", command);
+            customCommands[command.toLowerCase()](newTerminal, []);
+          } else {
+            newTerminal.sendText(command);
+          }
         });
       } else {
         existingTerminal.show();
         commands.forEach((command: string) => {
-          if (customCommands[command]) {
+          if (customCommands[command.toLowerCase()]) {
             console.log("custom command found", command);
-            customCommands[command](existingTerminal, []);
+            customCommands[command.toLowerCase()](existingTerminal, []);
           } else {
             existingTerminal.sendText(command);
           }
