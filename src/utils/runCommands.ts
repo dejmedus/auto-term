5739a5877d42191d9e0c3521d8a8b299115d7290
Julia <email96123@gmail.com>
2024-04-04 12:54:49 -0600

    chain commands to wait for completion && rename key name to tab

--- src/utils/runCommands.ts
+++ src/utils/runCommands.ts
@@ -11,7 +11,14 @@ export default function runCommands(tabConfig: any) {
     }
 
     tabConfig.forEach((config) => {
-      const terminalName = config.name;
+      const terminalName = config.tab || config.name;
+
+      // if (!config.tab && config.name) {
+      //   vscode.window.showWarningMessage(
+      //     "Please use 'tab' instead of 'name' in the configuration file. (deprecated 0.0.6)"
+      //   );
+      // }
+
       const commands = config.commands;
 
       const existingTerminal = vscode.window.terminals.find(
@@ -34,11 +41,21 @@ export default function runCommands(tabConfig: any) {
   }
 }
 
-function runCommandLoop(commands: any, terminal: any) {
+function runCommandLoop(commands: string[], terminal: vscode.Terminal) {
   terminal.show();
+
+  commands = sortCommands(commands);
+
   commands.forEach((command: string) => {
     if (command.startsWith("*")) {
-      const [commandType, ...args] = command.toLowerCase().split(" ");
+      const [commandType, ...args] = command.split(" ");
+
+      if (!customCommands[commandType.toLowerCase()]) {
+        vscode.window.showErrorMessage(
+          `Command ${commandType} not found in custom commands.`
+        );
+        return;
+      }
 
       customCommands[commandType.toLowerCase()](terminal, args);
     } else {
@@ -46,3 +63,34 @@ function runCommandLoop(commands: any, terminal: any) {
     }
   });
 }
+
+const sortCommands = (commands: string[]) =>
+  commands.reduce(
+    (acc: { result: string[]; curString: string }, command, i) => {
+      if (command.startsWith("*")) {
+        // if custom command
+        if (acc.curString) {
+          acc.result.push(acc.curString.trim());
+          acc.curString = "";
+        }
+        acc.result.push(command);
+      } else {
+        acc.curString += command;
+        if (!commands[i + 1] || commands[i + 1].startsWith("*")) {
+          // if the final command
+          acc.result.push(acc.curString.trim());
+          acc.curString = "";
+          // } else if (commands[i + 1].startsWith("*")) {
+          //   // if the next command is a custom command
+          //   // acc.curString += " && clear";
+          //   acc.result.push(acc.curString.trim());
+          //   acc.curString = "";
+        } else {
+          // if the next command is a command
+          acc.curString += " && ";
+        }
+      }
+      return acc;
+    },
+    { result: [], curString: "" }
+  ).result;
