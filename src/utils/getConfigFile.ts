9a021cb5b999d736721920ae8a87d5d1693703c4
Julia <email96123@gmail.com>
2024-04-14 12:09:39 -0600

    add param to prevent no workspace error message

--- src/utils/getConfigFile.ts
+++ src/utils/getConfigFile.ts
@@ -5,12 +5,18 @@ import fs from "fs";
 /**
  * This function retrieves the configuration file.
  * @param allowMissingConfig - If true, no error messages will be displayed when the configuration file is missing. Defaults to false.
+ * @param allowMissingWorkspace - If true, no error messages will be displayed when a workspace is missing. Defaults to false.
  */
-export default function getConfigFile(allowMissingConfig: boolean = false) {
+export default function getConfigFile(
+  allowMissingConfig: boolean = false,
+  allowMissingWorkspace: boolean = false
+) {
   const workspaceFolder = vscode.workspace.workspaceFolders?.[0];
 
   if (!workspaceFolder) {
-    vscode.window.showErrorMessage("No workspace found.");
+    if (!allowMissingWorkspace) {
+      vscode.window.showErrorMessage("No workspace found.");
+    }
     return;
   }
 
