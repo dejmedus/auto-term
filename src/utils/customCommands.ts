15780729915f57f3a26dba115659735919c9c254
Julia <email96123@gmail.com>
2024-05-02 14:29:34 -0600

    listen for command completion before continuing command loop

--- src/utils/customCommands.ts
+++ src/utils/customCommands.ts
@@ -1,4 +1,5 @@
 import vscode from "vscode";
+import { sendCommandToShell } from "./shellIntegration";
 
 const customCommands: {
   [key: string]: (terminal: any, args: string[]) => Promise<void>;
@@ -9,24 +10,38 @@ const customCommands: {
   "*echo": echo,
 };
 
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
+
 async function handleStop(terminal: vscode.Terminal) {
   const pid = await terminal.processId;
-  terminal.sendText(`kill -SIGINT ${pid}`);
+  await sendCommandToShell(`kill -SIGINT ${pid}`, terminal);
 }
 
 async function handleClose(terminal: vscode.Terminal) {
-  terminal.dispose();
+  await new Promise<void>((resolve) => {
+    const disposable = vscode.window.onDidCloseTerminal((closedTerminal) => {
+      if (closedTerminal === terminal) {
+        disposable.dispose();
+        resolve();
+      }
+    });
+
+    terminal.dispose();
+  });
 }
 
 async function echo(terminal: vscode.Terminal, args: string[]) {
   const message = args.join(" ");
-  terminal.sendText(`echo "${message}"\n`);
+  await sendCommandToShell(`echo "${message}"\n`, terminal);
 }
 
 async function alertMessage(terminal: vscode.Terminal, args: string[]) {
   // vscode only allows 3 notifications open at a time
   const message = args.join(" ");
   vscode.window.showInformationMessage(message);
+  await delay(1000);
 }
 
 export default customCommands;
