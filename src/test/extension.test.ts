1aa2d829cce050fab198b77a4186da694f8c5b99
Julia <email96123@gmail.com>
2024-09-22 16:56:20 -0600

    add terminal helper tests

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,14 +1,80 @@
-import * as assert from "assert";
-import * as vscode from "vscode";
+import assert from "assert";
+import { workspace, commands } from "vscode";
+import { spy, assert as sinonAssert } from "sinon";
+import { newTerminalWrap } from "./newTerminalWrap";
 
-import * as fs from "fs";
-import * as path from "path";
+import fs from "fs";
+import path from "path";
+
+import { runInCurrentTerminal, runCommand } from "../utils/terminalHelpers";
 
 import { getTemplateFile } from "../commands/getTemplate";
 
-suite("Extension Test Suite", () => {
+suite("terminal helpers", () => {
+  test(
+    "new terminal",
+    newTerminalWrap(async (terminal, shellIntegration) => {
+      const commands = ["echo 'woww tests'"];
+
+      const shellIntegrationSpy = spy(shellIntegration, "executeCommand");
+
+      for (const command of commands) {
+        await runCommand(terminal, command);
+      }
+
+      sinonAssert.calledWith(shellIntegrationSpy, "echo 'woww tests'");
+
+      shellIntegrationSpy.restore();
+    })
+  );
+
+  test(
+    "command loop",
+    newTerminalWrap(async (terminal, shellIntegration) => {
+      const shellIntegrationSpy = spy(shellIntegration, "executeCommand");
+
+      await runCommand(terminal, "echo 'hi'");
+      sinonAssert.calledWith(shellIntegrationSpy, "echo 'hi'");
+
+      const commands = [
+        "echo 'woww'",
+        "*delay 5000",
+        "npm init -y",
+        "echo 'POTATO'",
+      ];
+      await runInCurrentTerminal(terminal, commands);
+
+      sinonAssert.calledWith(shellIntegrationSpy, "echo 'woww'");
+      sinonAssert.calledWith(shellIntegrationSpy, "npm init -y");
+      sinonAssert.calledWith(shellIntegrationSpy, "echo 'POTATO'");
+
+      shellIntegrationSpy.restore();
+    }, "command loop")
+  );
+  test("command types", async () => {
+    newTerminalWrap(async (terminal, shellIntegration) => {
+      const shellIntegrationSpy = spy(shellIntegration, "executeCommand");
+
+      let returnValue = await runCommand(terminal, "*echo 'custom echo'");
+      assert.equal(returnValue.type, "execution");
+
+      returnValue = await runCommand(terminal, "*alert");
+      assert.equal(returnValue.type, "continue");
+
+      returnValue = await runCommand(terminal, "*potato");
+      assert.equal(returnValue.type, "error");
+
+      returnValue = await runCommand(terminal, "*stop");
+      assert.equal(returnValue.type, "cancel");
+
+      shellIntegrationSpy.restore();
+    }, "command type check");
+  });
+});
+
+suite("Config", () => {
   test("settings config is found", () => {
-    const runOpenCommandsOnStartup: boolean | undefined = vscode.workspace
+    const runOpenCommandsOnStartup: boolean | undefined = workspace
       .getConfiguration("autoTerminal")
       .get("runOpenCommandsOnStartup");
 
@@ -16,16 +82,16 @@ suite("Extension Test Suite", () => {
       runOpenCommandsOnStartup === true || runOpenCommandsOnStartup === false
     );
 
-    const customTemplates = vscode.workspace
+    // check
+    const customTemplates = workspace
       .getConfiguration("autoTerminal")
       .get("customTemplates");
-    assert.ok(
-      customTemplates === undefined || typeof customTemplates === "object"
-    );
+
+    assert.ok(typeof customTemplates === "object");
   });
 
   test("extension commands are found", () => {
-    vscode.commands.getCommands(true).then((commands) => {
+    commands.getCommands(true).then((commands) => {
       const templateCommandExists = commands.includes("extension.getTemplate");
       const guideCommandExists = commands.includes("extension.showUsageGuide");
       const actionCommandExists = commands.includes("extension.action");
